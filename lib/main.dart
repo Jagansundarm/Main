4f93284dcdae31e385dfa312672a6d2dfbfe8de6
Jagansundarm <jagansundar.m30@gmail.com>
2025-07-29 15:39:07 +0530

    new

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+runApp(MaterialApp(
+  home: Scaffold(
+    appBar:AppBar(
+       title:Text("Hellow world"),
+          backgroundColor: const Color.fromARGB(255, 182, 5, 206),
+    centerTitle: true,
+    ),
+  )));
+}
+
